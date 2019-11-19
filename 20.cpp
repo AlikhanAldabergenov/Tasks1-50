40c0b73f0aff2d3b539cdd371bd6a89aaa255cf9
AlikhanAldabergenov <57914945+AlikhanAldabergenov@users.noreply.github.com>
2019-11-19 11:57:24 +0600

    Add files via upload

---
+++ 20.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#define _USE_MATH_DEFINES 
+using namespace std;
+int main() {
+	double a,b,c,d;
+	a=647;
+	b=170;
+	c=30;
+	d=(647/30)*(170/30);
+	cout<<d;
+	return 0;
+}
