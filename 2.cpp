0a6874ceb34a8b66e0160ba9205c7b38ea4b2ed3
AlikhanAldabergenov <57914945+AlikhanAldabergenov@users.noreply.github.com>
2019-11-19 12:41:42 +0600

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-  cout<<"Monday"<<endl<<"November"<<endl<<"2019"<<endl<<"Yernur";
+  cout<<"Monday"<<endl<<"November"<<endl<<"2019"<<endl<<"Alikhan";
   return 0;
  }
